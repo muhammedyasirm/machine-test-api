c6c391b35aa2775d916f6e390eb0059714aa516c
muhammedyasirm <87080246+muhammedyasirm@users.noreply.github.com>
2024-06-25 04:14:05 +0400

    Initial Commit
    => Setup api's for crup application
    => Migrate models for sql server

---
+++ Models/DonationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options) 
+            :base(options)
+        {
+        }
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
